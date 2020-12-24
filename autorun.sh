9183586537cb5e54911ca4453c818cf8070efdf9
Mikko Suhonen <miksuh@student.uef.fi>
2020-12-24 15:46:19 +0200

    update

--- autorun.sh
+++ autorun.sh
@@ -2,11 +2,11 @@
 
 xmodmap -e "keycode 49 = Scroll_Lock"
 xbindkeys &
-xset r rate 350 25
+xset r rate 350 30
 dunst -config $HOME/.config/dunst/dunstrc &
+
+[ -f $HOME/custom_autorun.sh ] && bash $HOME/custom_autorun.sh
 
 nitrogen --head=0 --set-zoom-fill $HOME/Pictures/taustakuvat/aika.jpg
 nitrogen --head=1 --set-zoom-fill $HOME/Pictures/taustakuvat/aika.jpg
 nitrogen --head=2 --set-zoom-fill $HOME/Pictures/taustakuvat/aika.jpg
-
-[ -f $HOME/custom_autorun.sh ] && bash $HOME/custom_autorun.sh
