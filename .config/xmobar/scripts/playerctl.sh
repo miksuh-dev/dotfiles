854fd38cc28a01974efb272f8448f4fd83ed87e1
Mikko Suhonen <thesneikki@gmail.com>
2021-05-27 17:23:06 +0300

    Updated dotfiles

--- .config/xmobar/scripts/playerctl.sh
+++ .config/xmobar/scripts/playerctl.sh
@@ -17,7 +17,7 @@ if [ -x "$(command -v playerctl)" ]; then
 
 
         if [[ ${#text} -gt 48 ]] ; then
-            echo $text | cut -c1-45 | xargs -I{} echo -n "Playing: {}... |"
+            echo $text | cut -c1-45 | xargs -I{} -0 echo -n "Playing: {}... |"
         else
             echo $text | xargs -I{} echo -n "Playing: {} |"
         fi
