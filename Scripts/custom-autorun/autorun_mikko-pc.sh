09906db14c2a66886a6571c4b505f15817f12608
Mikko Suhonen <thesneikki@gmail.com>
2021-06-05 20:24:20 +0300

    Updated dotfiles

--- Scripts/custom-autorun/autorun_mikko-pc.sh
+++ Scripts/custom-autorun/autorun_mikko-pc.sh
@@ -6,9 +6,13 @@ $HOME/Programs/FreeFileSync/fileserver_sync.sh &
 #xrandr --output DP-4 --mode 2560x1440 --panning 2560x1440+2560+0 --rate 144 --primary
 #xrandr --output DP-2 --mode 2560x1440 --panning 2560x1440+5120+0 --rate 144
 
+# xrandr --output DP-4 --mode 2560x1440 --rate 144 --rotate left
+# xrandr --output DP-0 --mode 2560x1440 --right-of DP-4 --rate 144 --primary
+# xrandr --output HDMI-0 --mode 2560x1440 --right-of DP-0 --rate 144
+
 xrandr --output DP-4 --mode 2560x1440 --rate 144 --rotate left
-xrandr --output DP-0 --mode 2560x1440 --right-of DP-4 --rate 144 --primary
-xrandr --output HDMI-0 --mode 2560x1440 --right-of DP-0 --rate 144
+xrandr --output DP-0 --mode 2560x1440  --pos 1440x560 --rate 144 --primary
+xrandr --output HDMI-0 --mode 2560x1440 --pos 4000x560 --rate 144
 
 # nitrogen --set-zoom-fill /home/miksuh/Pictures/taustakuvat/5120x1440/armenia_khustup_k13_annihilation_hayk-wallpaper-5120x1440.jpg
 /home/miksuh/Scripts/styli.sh -w 2560 -h 1440 -s nature
