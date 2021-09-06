a5f6bacd7bafefa28ac8e1c8fc2461899c622e04
Mikko Suhonen <mikko.suhonen@talentree.fi>
2021-09-06 14:50:20 +0300

    update

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -42,7 +42,7 @@ vim.o.ignorecase = true
 vim.o.termguicolors = true
 vim.o.clipboard='unnamedplus'
 
-
+vim.wo.signcolumn = 'yes' 
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.cursorline = true
