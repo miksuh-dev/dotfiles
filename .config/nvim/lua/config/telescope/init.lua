82f5c14be0a1519bc8a0269e3ae27e4a2c2691b3
Mikko Suhonen <mikko.suhonen@talentree.fi>
2021-09-27 08:51:31 +0300

    update

--- .config/nvim/lua/config/telescope/init.lua
+++ .config/nvim/lua/config/telescope/init.lua
@@ -67,3 +67,5 @@ vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>lua require("telescope.builtin"
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<CR>', { noremap = true, silent = true})
+
+vim.api.nvim_set_keymap('i', '<c-r>', '<cmd>lua require("telescope.builtin").registers()<CR>', { noremap = true, silent = true})
