7082ae060ee42d63e27481e35d0b7050f60a5252
Mikko Suhonen <thesneikki@gmail.com>
2021-10-20 17:13:52 +0300

    increase border

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -96,7 +96,7 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
-myBorderWidth   = 3
+myBorderWidth   = 4
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
