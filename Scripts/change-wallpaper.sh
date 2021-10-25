23d47d728442f5106c8143abdea1c1109bd44116
Mikko Suhonen <thesneikki@gmail.com>
2021-10-25 19:03:46 +0300

    change wallpaper based on current month

--- Scripts/change-wallpaper.sh
+++ Scripts/change-wallpaper.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-$HOME/Scripts/styli.sh -w 2560 -h 2560 -b bg-fill -s nature -n -m 3
+# $HOME/Scripts/styli.sh -w 2560 -h 2560 -b bg-fill -s nature -n -m 3
 
+declare -A months
+months[1]="january"
+months[2]="february"
+months[3]="march"
+months[4]="april"
+months[5]="may"
+months[6]="june"
+months[7]="july"
+months[8]="august"
+months[9]="september"
+months[10]="october"
+months[11]="november"
+months[12]="december"
 
+current_month=$(date +%m)
+month_string=${months[$current_month]}
+
+$HOME/Scripts/styli.sh -w 2560 -h 2560 -b bg-fill -s ${month_string} -n -m 3
