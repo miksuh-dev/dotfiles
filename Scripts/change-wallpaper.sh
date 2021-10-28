f06b5cdab1e1c8b94ddfbbfd4947ede88bcc2318
Mikko Suhonen <mikko.suhonen@talentree.fi>
2021-10-28 16:11:33 +0300

    update

--- Scripts/change-wallpaper.sh
+++ Scripts/change-wallpaper.sh
@@ -19,4 +19,8 @@ months[12]="december"
 current_month=$(date +%m)
 month_string=${months[$current_month]}
 
-$HOME/Scripts/styli.sh -w 2560 -h 2560 -b bg-fill -s ${month_string} -n -m 3
+$HOME/Scripts/styli.sh -w 2560 -h 2560 -b bg-fill -s ${month_string} -n -m 3 && exit 0
+
+nitrogen --set-zoom-fill --head=0 /home/miksuh/Pictures/taustakuvat/mountains_forest_trees_213094_1440x2560.jpg
+nitrogen --set-zoom-fill --head=1 /home/miksuh/Pictures/taustakuvat/mountains_forest_trees_213094_1440x2560.jpg
+nitrogen --set-zoom-fill --head=2 /home/miksuh/Pictures/taustakuvat/mountains_forest_trees_213094_1440x2560.jpg
