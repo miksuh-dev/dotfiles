e21d91291732cf78f157ed9752dd338dc1f13683
Mikko Suhonen <mikko.suhonen@talentree.fi>
2021-10-29 08:48:53 +0300

    update

---
+++ .config/nvim/lua/plugin/vim-tmux-navigator/load.lua
@@ -0,0 +1,10 @@
+vim.g.tmux_navigator_no_wrap = 1
+vim.g.tmux_navigator_no_mappings = 1
+
+local nnoremap = vim.keymap.nnoremap
+
+nnoremap({ '<C-H>', ':TmuxNavigateLeft<cr> ', silent = true })
+nnoremap({ '<C-J>', ':TmuxNavigateDown<cr>', silent = true })
+nnoremap({ '<C-K>', ':TmuxNavigateUp<cr>', silent = true })
+nnoremap({ '<C-L>', ':TmuxNavigateRight<cr>', silent = true })
+nnoremap({ '<C-Space>', ':TmuxNavigatePrevious<cr>', silent = true })
