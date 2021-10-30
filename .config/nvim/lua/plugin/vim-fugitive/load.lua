6e7432a90d2fa56ac92e99ab9f669a6f716c02cc
Mikko Suhonen <thesneikki@gmail.com>
2021-10-30 15:44:35 +0300

    add binding to Gedit

--- .config/nvim/lua/plugin/vim-fugitive/load.lua
+++ .config/nvim/lua/plugin/vim-fugitive/load.lua
@@ -4,5 +4,6 @@ local nnoremap = vim.keymap.nnoremap
 nnoremap({ '<leader>gf', ':diffget //2<CR>' })
 nnoremap({ '<leader>gj', ':diffget //3<CR>' })
 nnoremap({ '<leader>gs', ':G<CR>', silent = true })
+nnoremap({ '<leader>ge', ':Gedit<CR>', silent = true })
 nnoremap({ '<leader>gb', ':Git blame<CR>', silent = true })
 nnoremap({ '<leader>gd', ':Gvdiffsplit<CR>', silent = true })
