648c23db532cb58e7533df675aed7aa3b74b512d
Mikko Suhonen <thesneikki@gmail.com>
2021-11-04 18:22:51 +0200

    fallback to local folder

--- Scripts/change-wallpaper.sh
+++ Scripts/change-wallpaper.sh
@@ -19,8 +19,8 @@ months[12]="december"
 current_month=$(date +%m)
 month_string=${months[$current_month]}
 
-$HOME/Scripts/styli.sh -w 2560 -h 2560 -b bg-fill -s ${month_string} -n -m 3 && exit 0
+$HOME/Scripts/styli.sh -w 2560 -h 2560 -b bg-fill -s ${month_string} -m 3 && exit 0
 
-nitrogen --set-zoom-fill --head=0 /home/miksuh/Pictures/taustakuvat/mountains_forest_trees_213094_1440x2560.jpg
-nitrogen --set-zoom-fill --head=1 /home/miksuh/Pictures/taustakuvat/mountains_forest_trees_213094_1440x2560.jpg
-nitrogen --set-zoom-fill --head=2 /home/miksuh/Pictures/taustakuvat/mountains_forest_trees_213094_1440x2560.jpg
+nitrogen --set-zoom-fill --random $HOME/Pictures/taustakuvat/7680x1400/ && exit 0
+nitrogen --set-zoom-fill --random $HOME/Pictures/taustakuvat/ && exit 0
+
