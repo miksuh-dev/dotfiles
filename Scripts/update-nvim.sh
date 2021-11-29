3bdef61ec1f0f80e3019d10b09711ebfae91a270
Mikko Suhonen <thesneikki@gmail.com>
2021-11-29 17:33:20 +0200

    fix nvim-update

--- Scripts/update-nvim.sh
+++ Scripts/update-nvim.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-wget -P $HOME/Downloads https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage || exit 1
-chmod +x $HOME/Downloads/nvim/nvim.appimage || exit 1
-mv $HOME/Downloads/nvim/nvim.appimage $HOME/Downloads/nvim/nvim || exit 1
-
-sudo mv $HOME/Downloads/nvim/nvim /usr/local/bin/nvim && echo "Success" || echo "Failed"
+rm -f ./nvim
+wget -P ./ https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage || exit 1
+chmod +x ./nvim.appimage
+mv ./nvim.appimage ./nvim
+sudo mv ./nvim /usr/local/bin/nvim && echo "Success" || echo "Failed"
