800d45d552ba559cd174bc223c47800ce68f65f3
Mikko Suhonen <thesneikki@gmail.com>
2021-12-09 17:46:48 +0200

    add yankhighlight highglight group

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -5,6 +5,6 @@ vim.cmd([[runtime plugin/astronauta.vim]])
 vim.cmd([[
   augroup highlight_yank
     autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=200 }
+    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='YankVisual', timeout=200 }
   augroup END
 ]])
