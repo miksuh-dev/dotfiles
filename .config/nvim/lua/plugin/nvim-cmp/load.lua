7bb0fee29ef97c751568fb760262e6c7fc6f43e2
Mikko Suhonen <thesneikki@gmail.com>
2021-12-09 21:13:08 +0200

    cmp remove <c-e> mapping

--- .config/nvim/lua/plugin/nvim-cmp/load.lua
+++ .config/nvim/lua/plugin/nvim-cmp/load.lua
@@ -48,10 +48,6 @@ cmp.setup({
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
