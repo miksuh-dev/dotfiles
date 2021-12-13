f8b8b4798b7deb4c4f423624afae480c5ea7ae77
Mikko Suhonen <thesneikki@gmail.com>
2021-12-13 19:20:25 +0200

    decrease border to 3

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -91,7 +91,7 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
-myBorderWidth   = 4
+myBorderWidth   = 3
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
