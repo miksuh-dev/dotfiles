9d4b7376e1842e130437d6c4a593f4c61ed479c9
Mikko Suhonen <thesneikki@gmail.com>
2021-12-27 20:31:17 +0200

    change vimspector restart bind

--- .config/nvim/lua/plugin/vimspector/load.lua
+++ .config/nvim/lua/plugin/vimspector/load.lua
@@ -9,12 +9,12 @@ local xmap = vim.keymap.xmap
 nnoremap({ '<Leader>dd', ':lua require("plugin.vimspector.util").launch()<CR>', silent = true })
 -- nnoremap({ '<Leader>dd', ':call vimspector#Launch()<CR>' })
 nnoremap({ '<Leader>de', ':call vimspector#Reset()<CR>' })
+nmap({ '<Leader>dr', '<Plug>VimspectorRestart' })
 nnoremap({ '<Leader>dc', ':call vimspector#Continue()<CR>' })
 
 nnoremap({ '<Leader>dt', ':call vimspector#ToggleBreakpoint()<CR>' })
 nnoremap({ '<Leader>dT', ':call vimspector#ClearBreakpoints()<CR>' })
 
-nmap({ '<Leader>dk', '<Plug>VimspectorRestart' })
 nmap({ '<Leader>dh', '<Plug>VimspectorStepOut' })
 nmap({ '<Leader>dl', '<Plug>VimspectorStepInto' })
 nmap({ '<Leader>dj', '<Plug>VimspectorStepOver' })
