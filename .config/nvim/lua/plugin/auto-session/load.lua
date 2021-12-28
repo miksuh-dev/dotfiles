266f41e9f74b4470249c73ec45e30c5ac461cf8a
Mikko Suhonen <thesneikki@gmail.com>
2021-12-28 18:48:03 +0200

    set auto-session log level to error

--- .config/nvim/lua/plugin/auto-session/load.lua
+++ .config/nvim/lua/plugin/auto-session/load.lua
@@ -2,7 +2,7 @@
 vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal'
 
 local opts = {
-  log_level = 'info',
+  log_level = 'error',
   auto_session_enable_last_session = false,
   auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
   auto_session_enabled = true,
