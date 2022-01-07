3c3362fab7a0bf6b1926071929d71cd5f426744d
Mikko Suhonen <mikko.suhonen@talentree.fi>
2022-01-07 13:08:41 +0200

    allow toggling git blame and status

--- .config/nvim/lua/plugin/vim-fugitive/load.lua
+++ .config/nvim/lua/plugin/vim-fugitive/load.lua
@@ -2,7 +2,32 @@ local nnoremap = vim.keymap.nnoremap
 
 nnoremap({ '<leader>gf', ':diffget //2<CR>' })
 nnoremap({ '<leader>gj', ':diffget //3<CR>' })
-nnoremap({ '<leader>gs', ':G<CR>', silent = true })
-nnoremap({ '<leader>ge', ':Gedit<CR>', silent = true })
-nnoremap({ '<leader>gb', ':Git blame<CR>', silent = true })
 nnoremap({ '<leader>gd', ':Gvdiffsplit<CR>', silent = true })
+nnoremap({ '<leader>gs', ':G<CR>', silent = true })
+
+nnoremap({ '<leader>gb', ':Git blame<CR>', silent = true })
+nnoremap({ '<leader>ge', ':Gedit<CR>', silent = true })
+
+nnoremap({
+  '<leader>gs',
+  function()
+    if vim.bo.filetype ~= 'fugitive' then
+      vim.cmd(':G')
+    else
+      vim.cmd(':q')
+    end
+  end,
+  silent = true,
+})
+nnoremap({
+  '<leader>gb',
+  function()
+    if vim.bo.filetype ~= 'fugitiveblame' then
+      vim.cmd(':Git blame')
+    else
+      vim.cmd(':q')
+    end
+  end,
+
+  silent = true,
+})
