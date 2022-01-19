6a096f589de710ea7804cbb8a530a19b84052f6d
Mikko Suhonen <thesneikki@gmail.com>
2022-01-19 20:08:45 +0200

    fix wilder colors

--- .config/nvim/lua/plugin/wilder/load.lua
+++ .config/nvim/lua/plugin/wilder/load.lua
@@ -1,4 +1,5 @@
 local wilder = require('wilder')
+local theme = require('theme.monokai').classic
 
 wilder.setup({
   modes = { ':', '/', '?' },
@@ -7,34 +8,32 @@ wilder.setup({
   accept_key = '<Down>',
   reject_key = '<Up>',
 })
---
+
 wilder.set_option(
   'renderer',
   wilder.popupmenu_renderer({
     highlighter = wilder.basic_highlighter(),
-    -- highlights = {
-    --   accent = wilder.make_hl('WilderAccent', 'Pmenu', {
-    --     {},
-    --     {},
-    --     { theme.aqua },
-    --   }),
-    -- },
+    highlights = {
+      default = wilder.make_hl('WilderDefault', '', {
+        {},
+        {},
+        { theme.base7, theme.base2, 'nocombine' },
+      }),
+      accent = wilder.make_hl('WilderAccent', '', {
+        {},
+        {},
+        { theme.white, theme.base2, 'nocombine' },
+      }),
+      selected = wilder.make_hl('WilderSelected', '', {
+        {},
+        {},
+        { theme.white, theme.blue, 'bold' },
+      }),
+      selected_accent = wilder.make_hl('WilderSelectedAccent', '', {
+        {},
+        {},
+        { theme.white, theme.blue, 'bold' },
+      }),
+    },
   })
 )
-
--- vim.cmd([[
---   call wilder#setup({
---     \ 'modes': [':', '/', '?'],
---     \ 'next_key': '<Tab>',
---     \ 'previous_key': '<S-Tab>',
---     \ 'accept_key': '<Down>',
---     \ 'reject_key': '<Up>',
---     \ })
---
---   call wilder#set_option('renderer', wilder#popupmenu_renderer({
---     \ 'highlighter': wilder#basic_highlighter(),
---     \ 'highlights': {
---     \   'accent': wilder#make_hl('WilderAccent', 'Pmenu', [{}, {}, {'foreground': '#f4468f'}]),
---     \ },
---     \ }))
---  ]])
