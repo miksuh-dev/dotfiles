bf3f9864d5be52930fbe15f8bf936484886a25dd
Mikko Suhonen <thesneikki@gmail.com>
2022-02-17 18:12:52 +0200

    improve luasnips

--- .config/nvim/lua/plugin/luasnips/load.lua
+++ .config/nvim/lua/plugin/luasnips/load.lua
@@ -1,4 +1,5 @@
 local ls = require('luasnip')
+local types = require('luasnip.util.types')
 
 local function get_snippet_paths()
   local util = require('common.util')
@@ -25,6 +26,42 @@ local function get_snippet_paths()
 
   return paths
 end
+
+ls.config.set_config({
+  history = true,
+  updateevents = 'TextChanged,TextChangedI',
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { '<-', 'Error' } },
+      },
+    },
+  },
+})
+
+vim.keymap.set({ 'i', 's' }, '<c-l>', function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end, {
+  silent = true,
+})
+
+vim.keymap.set({ 'i', 's' }, '<c-k>', function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, {
+  silent = true,
+})
+
+vim.keymap.set({ 'i', 's' }, '<c-j>', function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, {
+  silent = true,
+})
 
 -- use javascriptreact on javascript files
 ls.filetype_set('javascript', { 'javascriptreact' })
