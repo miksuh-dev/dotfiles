410f0c9b1e4129d777b9d05ce949bb5709d9bcda
Mikko Suhonen <mikko.suhonen@talentree.fi>
2022-03-07 15:50:59 +0200

    autofold on .md

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -5,3 +5,12 @@ vim.cmd([[
     au TextYankPost * silent! lua vim.highlight.on_yank { higroup='YankVisual', timeout=250 }
   augroup END
 ]])
+
+-- Toggle word wrapping in markdown files
+-- TODO Also put this on txt files
+vim.cmd([[
+  augroup Markdown
+    autocmd!
+    autocmd FileType markdown set wrap
+  augroup END
+]])
