7939cc6ead3669113c660b029b365f981f0df5c4
Mikko Suhonen <thesneikki@gmail.com>
2022-03-10 17:12:20 +0200

    tabline prevent overflow

--- .config/nvim/lua/plugin/lualine/tabline.lua
+++ .config/nvim/lua/plugin/lualine/tabline.lua
@@ -1,24 +1,30 @@
 local M = {}
 local util = require('common.util')
 
--- TODO Add too long tabline handling
-local function splitFilename(strFilename)
-  if strFilename:match('/') then
-    local table = util.split(strFilename, '/')
+local function shorten_filename(path)
+  if path:match('/') then
+    local table = util.split(path, '/')
     local length = util.tablelength(table)
 
     return table[length - 1] .. '/' .. table[length]
   end
 
-  return strFilename
+  return path
 end
 
-local function harpoon_mark_styled(index, filename)
-  if vim.fn.expand('%') == filename then
-    return '%#TablineSel# ' .. index .. ':' .. splitFilename(filename) .. ' %#Tabline#'
-  end
+local function is_selected(filename)
+  return vim.fn.expand('%') == filename
+end
 
-  return '%#Tabline# ' .. index .. ':' .. splitFilename(filename) .. ' '
+local function get_styled_text(filename, text)
+  if is_selected(filename) then
+    return '%#TablineSel#' .. text .. '%#Tabline#'
+  end
+  return '%#Tabline#' .. text
+end
+
+local function get_text_content(index, text)
+  return ' ' .. index .. ':' .. text .. ' '
 end
 
 M.alt_file = function()
@@ -30,8 +36,20 @@ M.harpoon_marks = function()
   local harpoon_marks = require('harpoon').get_mark_config().marks
 
   local t = {}
+  local window_width = vim.api.nvim_list_uis()[1].width
+
+  local length = 0
   for key, value in ipairs(harpoon_marks) do
-    t[#t + 1] = harpoon_mark_styled(key, tostring(value.filename))
+    local filename = value.filename
+    local text = get_text_content(key, shorten_filename(filename))
+
+    -- Prevent overflow
+    length = length + #text
+    if length >= window_width then
+      break
+    end
+
+    t[#t + 1] = get_styled_text(filename, text)
   end
   return '%#TablineFill#' .. table.concat(t, ' ')
 end
