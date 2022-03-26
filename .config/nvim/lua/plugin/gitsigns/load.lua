005447b705ff305b2d7b337c441a47afeb90946e
Mikko Suhonen <thesneikki@gmail.com>
2022-03-26 11:38:52 +0200

    change max length gitsigns

--- .config/nvim/lua/plugin/gitsigns/load.lua
+++ .config/nvim/lua/plugin/gitsigns/load.lua
@@ -88,7 +88,7 @@ require('gitsigns').setup({
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
-  max_file_length = 40000,
+  max_file_length = 10000,
   preview_config = {
     -- Options passed to nvim_open_win
     border = 'single',
