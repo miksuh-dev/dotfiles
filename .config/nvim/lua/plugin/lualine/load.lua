f05d77b808c93709da15f74a7c9e9a584397b4ec
Mikko Suhonen <thesneikki@gmail.com>
2022-03-27 12:30:27 +0300

    lualine gps refactoring

--- .config/nvim/lua/plugin/lualine/load.lua
+++ .config/nvim/lua/plugin/lualine/load.lua
@@ -1,18 +1,6 @@
 local theme = require('plugin.lualine.theme.dark')
 local tabline = require('plugin.lualine.tabline')
-
 local gps = require('nvim-gps')
-
--- TODO Use this to make console.log to output require('nvim-gps').get_data()
-local function get_gps()
-  if gps.is_available() then
-    local location = gps.get_location()
-    if location ~= '' then
-      return '> ' .. location
-    end
-  end
-  return ''
-end
 
 require('lualine').setup({
   options = {
@@ -44,7 +32,11 @@ require('lualine').setup({
         },
       },
       {
-        get_gps,
+        gps.get_location,
+        cond = gps.is_available,
+        fmt = function(str)
+          return str ~= '' and '> ' .. str or ''
+        end,
       },
     },
     lualine_x = {
@@ -74,57 +66,61 @@ require('lualine').setup({
     lualine_z = { 'location' },
   },
   inactive_sections = {
-    lualine_a = {
-      {
-        'mode',
-        fmt = function(str)
-          return str:sub(1, 1) .. str:sub(2):lower()
-        end,
-      },
-    },
-    lualine_b = { 'branch' },
-    lualine_c = {
-      {
-        'filename',
-        file_status = true,
-        path = 1,
-        padding = {
-          left = 1,
-          right = 0,
-        },
-      },
-      {
-        get_gps,
-      },
-    },
-    lualine_x = {
-      -- 'encoding',
-      {
-        'filetype',
-        colored = false,
-      },
-    },
-    lualine_y = {
-      {
-        'diagnostics',
-        sources = { 'nvim_diagnostic' },
-        sections = { 'error', 'warn', 'info', 'hint' },
-        symbols = {
-          warn = 'W',
-          error = 'E',
-          info = 'I',
-          hint = 'H',
-        },
-        diagnostics_color = {
-          error = 'DiagnosticError',
-          warn = 'DiagnosticWarn',
-          info = 'DiagnosticInfo',
-          hint = 'DiagnosticHint',
-        },
-        update_in_insert = false,
-      },
-    },
-    lualine_z = { 'location' },
+    -- lualine_a = {
+    --   {
+    --     'mode',
+    --     fmt = function(str)
+    --       return str:sub(1, 1) .. str:sub(2):lower()
+    --     end,
+    --   },
+    -- },
+    -- lualine_b = { 'branch' },
+    -- lualine_c = {
+    --   {
+    --     'filename',
+    --     file_status = true,
+    --     path = 1,
+    --     padding = {
+    --       left = 1,
+    --       right = 0,
+    --     },
+    --   },
+    --   {
+    --     gps.get_location,
+    --     cond = gps.is_available,
+    --     fmt = function(str)
+    --       return str ~= '' and '> ' .. str or ''
+    --     end,
+    --   },
+    -- },
+    -- lualine_x = {
+    --   -- 'encoding',
+    --   {
+    --     'filetype',
+    --     colored = false,
+    --   },
+    -- },
+    -- lualine_y = {
+    --   {
+    --     'diagnostics',
+    --     sources = { 'nvim_diagnostic' },
+    --     sections = { 'error', 'warn', 'info', 'hint' },
+    --     symbols = {
+    --       warn = 'W',
+    --       error = 'E',
+    --       info = 'I',
+    --       hint = 'H',
+    --     },
+    --     diagnostics_color = {
+    --       error = 'DiagnosticError',
+    --       warn = 'DiagnosticWarn',
+    --       info = 'DiagnosticInfo',
+    --       hint = 'DiagnosticHint',
+    --     },
+    --     update_in_insert = false,
+    --   },
+    -- },
+    -- lualine_z = { 'location' },
   },
   tabline = {
     lualine_a = {},
