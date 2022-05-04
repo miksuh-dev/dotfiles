e352688d4af6d0f3bec9d34ad8911e39903c3214
Mikko Suhonen <mikko.suhonen@talentree.fi>
2022-05-04 09:59:44 +0300

    fix cmp not showing with copilot

--- .config/nvim/lua/plugin/copilot/init.lua
+++ .config/nvim/lua/plugin/copilot/init.lua
@@ -1,6 +1,7 @@
 vim.g.copilot_no_maps = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ''
+vim.g.copilot_hide_during_completion = false
 
 vim.g.copilot_filetypes = {
   ['*'] = true,
