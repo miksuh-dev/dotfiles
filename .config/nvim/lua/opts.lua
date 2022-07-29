d172c6dc8bec514bc624f95d1de6943161f2a98e
Mikko Suhonen <miksuh.dev@gmail.com>
2022-07-29 16:35:22 +0300

    refactor: opts to lua

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -62,10 +62,8 @@ vim.g.loaded_python_provider = 0
 vim.g.lazyredraw = false
 vim.g.netrw_altfile = 1
 
--- TODO Find lua alternative
-vim.cmd([[
-  set mouse=
-]])
+-- Disable mouse
+vim.opt.mouse = {}
 
 -- Disable folding as i don't use it and it ruins dadbod-ui result window
 vim.g.nofoldenable = true
@@ -75,4 +73,5 @@ vim.opt.listchars = {
   eol = '↵',
 }
 
-vim.cmd('set guicursor=')
+-- Disable cursor (always use block)
+vim.opt.guicursor = {}
