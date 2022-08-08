c282fbc1eb47237dd0bb2404388344a65bb3aa8e
Mikko Suhonen <mikko.suhonen@talentree.fi>
2022-08-08 16:14:39 +0300

    fix: run check of startup

--- Scripts/autorun/custom-autorun/autorun_miksuh-ThinkBook-14-G2-ITL.sh
+++ Scripts/autorun/custom-autorun/autorun_miksuh-ThinkBook-14-G2-ITL.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pidof mattermost-desktop >/dev/null || /usr/bin/mattermost-desktop &
+ps --no-headers -C mattermost-desktop -o args,state > /dev/null || /usr/bin/mattermost-desktop &
 pidof teams >/dev/null || /usr/bin/teams &
 pidof thunderbird >/dev/null || /usr/bin/thunderbird &
