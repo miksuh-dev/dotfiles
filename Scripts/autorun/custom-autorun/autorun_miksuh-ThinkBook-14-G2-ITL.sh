4f60456de857f9dfbee854952af0246dc5c9dcdd
Mikko Suhonen <mikko.suhonen@talentree.fi>
2022-08-31 16:14:01 +0300

    remove mattermost from startup

--- Scripts/autorun/custom-autorun/autorun_miksuh-ThinkBook-14-G2-ITL.sh
+++ Scripts/autorun/custom-autorun/autorun_miksuh-ThinkBook-14-G2-ITL.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-ps --no-headers -C mattermost-desktop -o args,state > /dev/null || /usr/bin/mattermost-desktop &
 pidof teams >/dev/null || /usr/bin/teams &
 pidof thunderbird >/dev/null || /usr/bin/thunderbird &
