9ec6e5c83f835602f53e8dd6ef6db0c68a9d12f5
Mikko Suhonen <miksuh.dev@gmail.com>
2023-02-27 16:06:08 +0200

    use markdownpreview without node

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -259,7 +259,9 @@ return require('packer').startup({
 
     use({
       'iamcco/markdown-preview.nvim',
-      run = 'cd app && npm install',
+      run = function()
+        vim.fn['mkdp#util#install']()
+      end,
       setup = function()
         vim.g.mkdp_filetypes = { 'markdown' }
       end,
