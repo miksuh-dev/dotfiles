075c4ae9f33182018b2e2e785bb1330b4acbd156
Mikko Suhonen <miksuh.dev@gmail.com>
2023-02-27 16:08:02 +0200

    autoload graphql lsp

--- .config/nvim/lua/plugin/nvim-lspconfig/language-server/graphql.lua
+++ .config/nvim/lua/plugin/nvim-lspconfig/language-server/graphql.lua
@@ -3,11 +3,9 @@ local root_dir_files = {
   '.git',
 }
 
-local filetypes = { 'graphql', 'javascript' }
+local filetypes = { 'graphql' }
 
 return function(config)
-  config.autostart = false
-
   config.filetypes = filetypes
   config.root_dir = function(fname)
     return require('lspconfig').util.root_pattern(root_dir_files)(fname) or vim.fn.getcwd()
