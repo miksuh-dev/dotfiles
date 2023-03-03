46b056fa98df49d73f13fb19bbd2d69c8ac764ca
Mikko Suhonen <miksuh.dev@gmail.com>
2023-03-03 18:52:55 +0200

    update set screens

--- Scripts/set-screens.sh
+++ Scripts/set-screens.sh
@@ -8,9 +8,9 @@ DP0=$(echo $XRANDR_OUTPUT | grep -q "DP-0 connected" ; echo $?)
 DP4=$(echo $XRANDR_OUTPUT | grep -q "DP-4 connected" ; echo $?)
 VGA0=$(echo $XRANDR_OUTPUT | grep -q "VGA-0 connected" ; echo $?)
 LVDS0=$(echo $XRANDR_OUTPUT | grep -q "LVDS-0 connected" ; echo $?)
-DP1=$(echo $XRANDR_OUTPUT | grep -q "DP-1 connected" ; echo $?)
-DP11=$(echo $XRANDR_OUTPUT | grep -q "DP-1-1 connected" ; echo $?)
-DP12=$(echo $XRANDR_OUTPUT | grep -q "DP-1-2 connected" ; echo $?)
+DP21=$(echo $XRANDR_OUTPUT | grep -q "DP-2-1 connected" ; echo $?)
+DP22=$(echo $XRANDR_OUTPUT | grep -q "DP-2-2 connected" ; echo $?)
+DP3=$(echo $XRANDR_OUTPUT | grep -q "DP-3 connected" ; echo $?)
 HDMI1=$(echo $XRANDR_OUTPUT | grep -q "HDMI-1 connected" ; echo $?)
 EDPI1=$(echo $XRANDR_OUTPUT | grep -q "eDP-1 connected" ; echo $?)
 
@@ -21,24 +21,23 @@ if [[ $DP4 -eq 0 && $DP0 -eq 0 && $HDMI0 -eq 0 ]]; then
     xrandr --output DP-0 --mode 2560x1440  --pos 1440x560 --rate 144 --primary
     xrandr --output HDMI-0 --mode 2560x1440 --pos 4000x560 --rate 144
   fi
-elif [[ $HDMI1 -eq 0 && $DP11 -eq 0 && $DP12 -eq 0 && $EDP1 -eq 0 ]]; then
+elif [[ $HDMI1 -eq 0 && $DP21 -eq 0 && $DP22 -eq 0 && $EDP1 -eq 0 ]]; then
   # Home screens - Work PC
-  if [ "$HOSTNAME" = "miksuh-ThinkBook-14-G2-ITL" ]; then
+  if [ "$MACHINE_ID" = "4cc9745ad9" ]; then
     xrandr --output eDP-1 --off
-    xrandr --output HDMI-1 --mode 2560x1440 --pos 0x0 --rate 60 --rotate left
-    xrandr --output DP-1-1 --mode 2560x1440  --pos 1440x560 --rate 60 --primary
-    xrandr --output DP-1-2 --mode 2560x1440 --pos 4000x560 --rate 60
+    xrandr --output HDMI-1 --mode 2560x1440 --rate 60 --rotate left
+    xrandr --output DP-2-1 --mode 2560x1440 --rate 60 --right-of HDMI-1 --primary
+    xrandr --output DP-2-2 --mode 2560x1440 --rate 60 --right-of DP-2-1
   fi
-elif [[ $EDPI1 -eq 0 && $DP1 -eq 0 && $HDMI1 -eq 0 ]]; then
+elif [[ $EDPI1 -eq 0 && $DP3 -eq 0 ]]; then
   # Work screens - Work PC
-  if [ "$HOSTNAME" = "miksuh-ThinkBook-14-G2-ITL" ]; then
-    xrandr --output eDP-1 --mode 1920x1080 --primary
-    xrandr --output HDMI-1 --mode 2560x1440 --right-of eDP-1
-    xrandr --output DP-1 --mode 1920x1080 --right-of HDMI-1
+  if [ "$MACHINE_ID" = "4cc9745ad9" ]; then
+    xrandr --output eDP-1 --mode 1920x1080
+    xrandr --output DP-3 --mode 3440x1440 --primary --right-of eDP-1
   fi
 elif [[ $EDPI1 -eq 0 ]]; then
   # Single screen - Work PC
-  if [ "$HOSTNAME" = "miksuh-ThinkBook-14-G2-ITL" ]; then
+  if [ "$MACHINE_ID" = "4cc9745ad9" ]; then
     xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rate 60
   fi
 fi
