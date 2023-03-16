cf528a8f4d26e07257ab6069b0c9b08225a320cd
Mikko Suhonen <miksuh.dev@gmail.com>
2023-03-16 19:36:20 +0200

    add harpoon next/prev tab bind

--- .config/nvim/lua/config/harpoon/init.lua
+++ .config/nvim/lua/config/harpoon/init.lua
@@ -1,135 +1,5 @@
 local map = vim.keymap.set
 
--- map('n', '<leader>h1', function()
---   require('harpoon.mark').set_current_at(1)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h2', function()
---   require('harpoon.mark').set_current_at(2)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h3', function()
---   require('harpoon.mark').set_current_at(3)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h4', function()
---   require('harpoon.mark').set_current_at(4)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h5', function()
---   require('harpoon.mark').set_current_at(5)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h6', function()
---   require('harpoon.mark').set_current_at(6)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h7', function()
---   require('harpoon.mark').set_current_at(7)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h8', function()
---   require('harpoon.mark').set_current_at(8)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h9', function()
---   require('harpoon.mark').set_current_at(9)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>h0', function()
---   require('harpoon.mark').set_current_at(10)
---   vim.cmd(':redrawtabline')
--- end, {
---   silent = true,
--- })
---
--- map('n', '<leader>hh', function()
---   require('harpoon.ui').toggle_quick_menu()
--- end, {
---   silent = true,
--- })
-
--- map('n', '_', function()
---   require('harpoon.ui').toggle_quick_menu()
--- end, {
---   silent = true,
--- })
-
--- map('n', '<leader>+', function()
---   require('harpoon.ui').toggle_quick_menu()
--- end, {
---   silent = true,
--- })
-
--- map('n', '<leader>1', function()
---   require('harpoon.ui').nav_file(1)
--- end, { silent = true })
---
--- map('n', '<leader>2', function()
---   require('harpoon.ui').nav_file(2)
--- end, { silent = true })
---
--- map('n', '<leader>3', function()
---   require('harpoon.ui').nav_file(3)
--- end, { silent = true })
---
--- map('n', '<leader>4', function()
---   require('harpoon.ui').nav_file(4)
--- end, { silent = true })
---
--- map('n', '<leader>5', function()
---   require('harpoon.ui').nav_file(5)
--- end, { silent = true })
---
--- map('n', '<leader>6', function()
---   require('harpoon.ui').nav_file(6)
--- end, { silent = true })
---
--- map('n', '<leader>7', function()
---   require('harpoon.ui').nav_file(7)
--- end, { silent = true })
---
--- map('n', '<leader>8', function()
---   require('harpoon.ui').nav_file(8)
--- end, { silent = true })
---
--- map('n', '<leader>9', function()
---   require('harpoon.ui').nav_file(9)
--- end, { silent = true })
---
--- map('n', '<leader>0', function()
---   require('harpoon.ui').nav_file(10)
--- end, { silent = true })
-
---- New bindings
-
 map('n', '<leader>H', function()
   require('harpoon.mark').add_file()
   vim.cmd(':redrawtabline')
@@ -251,4 +121,12 @@ end, { silent = true })
 
 map('n', "'p", function()
   require('harpoon.ui').nav_file(10)
+end, { silent = true })
+
+map('n', '<s-tab>', function()
+  require('harpoon.ui').nav_prev()
+end, { silent = true })
+
+map('n', '<tab>', function()
+  require('harpoon.ui').nav_next()
 end, { silent = true })
