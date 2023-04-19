9cd55e52c1a85f523076b6afb3fc7295bcf8d681
Mikko Suhonen <miksuh.dev@gmail.com>
2023-04-19 19:07:36 +0300

    add bind to navigate back to orignal cwd

--- .config/nvim/lua/config/neo-tree/init.lua
+++ .config/nvim/lua/config/neo-tree/init.lua
@@ -4,6 +4,13 @@
 
 local map = vim.keymap.set
 
+local function is_current_file_in_cwd()
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_cwd = vim.loop.cwd()
+
+  return current_file:sub(0, vim.fn.len(current_cwd)) == current_cwd
+end
+
 map('n', '-', function()
   -- if vim.bo.filetype == 'dbui' then
   --   return vim.cmd(':DBUIToggle')
@@ -15,14 +22,19 @@ map('n', '-', function()
 
   vim.fn.setreg('a', vim.fn.getreg('#'))
 
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local cwd = vim.loop.cwd()
-
-  local is_file_in_cwd = current_file:sub(0, vim.fn.len(cwd)) == cwd
-  if is_file_in_cwd then
+  if is_current_file_in_cwd() then
     vim.cmd(':Neotree reveal filesystem current')
   else
-    vim.cmd(':Neotree filesystem current')
+    vim.cmd(':Neotree filesystem current reveal_force_cwd')
+  end
+end)
+
+local original_cwd = vim.loop.cwd()
+map('n', '_', function()
+  if is_current_file_in_cwd() then
+    vim.cmd(':Neotree reveal filesystem current dir=' .. original_cwd)
+  else
+    vim.cmd(':Neotree filesystem current dir=' .. original_cwd)
   end
 end)
 
