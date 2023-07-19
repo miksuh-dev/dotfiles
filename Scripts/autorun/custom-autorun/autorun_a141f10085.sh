1507b400f6b55ae025ca6fc293fa23fdeb45a554
Mikko Suhonen <miksuh.dev@gmail.com>
2023-07-19 16:03:06 +0300

    update autorun for a141f10085

--- Scripts/autorun/custom-autorun/autorun_a141f10085.sh
+++ Scripts/autorun/custom-autorun/autorun_a141f10085.sh
@@ -4,5 +4,6 @@ nvidia-settings --load-config-only &
 
 $HOME/Programs/FreeFileSync/fileserver_sync.sh &
 
-/usr/bin/rambox &
-/usr/bin/mumble &
+pidof rambox >/dev/null || /usr/bin/rambox &
+pidof mumble >/dev/null || /usr/bin/mumble-lts &
+pgrep -f "python /usr/bin/twitch-indicator" >/dev/null || /usr/bin/twitch-indicator &
