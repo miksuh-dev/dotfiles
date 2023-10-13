03d57ddb99445502302e945fe86719991fa6f382
Mikko Suhonen <miksuh.dev@gmail.com>
2023-10-13 17:05:15 +0300

    fix stylua formatting, use async based on filetype

--- .config/nvim/lua/config/conform/load.lua
+++ .config/nvim/lua/config/conform/load.lua
@@ -1,5 +1,7 @@
 local prettier = { 'prettierd', 'prettier' }
 
+local slow_format_filetypes = { 'json', 'jsonc' }
+
 require('conform').setup({
   formatters_by_ft = {
     css = { prettier },
@@ -14,18 +16,37 @@ require('conform').setup({
     typescriptreact = { prettier },
     json = { prettier },
     jsonc = { prettier },
-    -- lua = { 'stylua' },
+    lua = { 'stylua' },
     markdown = { prettier },
     yaml = { prettier },
     vue = { prettier },
     rust = { 'rustfmt' },
     robot = { 'robotidy' },
   },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
+  formatters = {
+    stylua = {
+      command = 'stylua',
+      args = { '-s', '--indent-type', 'Spaces', '--indent-width', '2', '--quote-style', 'AutoPreferSingle', '-' },
+    },
   },
+  format_on_save = function(bufnr)
+    if slow_format_filetypes[vim.bo[bufnr].filetype] then
+      return
+    end
+    local function on_format(err)
+      if err and err:match('timeout$') then
+        slow_format_filetypes[vim.bo[bufnr].filetype] = true
+      end
+    end
+
+    return { timeout_ms = 200, lsp_fallback = true }, on_format
+  end,
+  format_after_save = function(bufnr)
+    if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+      return
+    end
+    return { lsp_fallback = true }
+  end,
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
