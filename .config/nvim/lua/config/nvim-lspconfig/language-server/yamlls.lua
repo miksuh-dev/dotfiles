45c279ddbea29d47f8de559990ac576938594f1d
Mikko Suhonen <miksuh.dev@gmail.com>
2023-11-17 17:56:34 +0200

    fix yamlls schemastore

--- .config/nvim/lua/config/nvim-lspconfig/language-server/yamlls.lua
+++ .config/nvim/lua/config/nvim-lspconfig/language-server/yamlls.lua
@@ -1,14 +1,14 @@
-local json_schemas = require('schemastore').json.schemas({})
-
-local yaml_schemas = {}
-vim.tbl_map(function(schema)
-  yaml_schemas[schema.url] = schema.fileMatch
-end, json_schemas)
-
 return function(config)
   config.settings = {
     yaml = {
-      schemas = yaml_schemas,
+      schemaStore = {
+        -- You must disable built-in schemaStore support if you want to use
+        -- this plugin and its advanced options like `ignore`.
+        enable = false,
+        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+        url = '',
+      },
+      schemas = require('schemastore').yaml.schemas(),
     },
   }
 
