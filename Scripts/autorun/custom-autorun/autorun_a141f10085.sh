4681a6419d446b8c15608dc31a6377db2c1fd358
Mikko Suhonen <miksuh.dev@gmail.com>
2023-12-09 20:36:43 +0200

    add obs sink autorun create for a141f10085

--- Scripts/autorun/custom-autorun/autorun_a141f10085.sh
+++ Scripts/autorun/custom-autorun/autorun_a141f10085.sh
@@ -6,4 +6,4 @@ $HOME/Programs/FreeFileSync/fileserver_sync.sh &
 
 pidof rambox >/dev/null || /usr/bin/rambox &
 pidof mumble >/dev/null || /usr/bin/mumble-lts &
-pgrep -f "python /usr/bin/twitch-indicator" >/dev/null || /usr/bin/twitch-indicator &
+pacmd list-sources | grep -e 'name: <obs.monitor>' || ~/Scripts/set-virtual-obs.sh
