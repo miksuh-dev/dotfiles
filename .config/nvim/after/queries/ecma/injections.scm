47d7045e55a9e62d4b59e215b76f5e84c2450c0f
Mikko Suhonen <miksuh.dev@gmail.com>
2024-01-30 19:05:30 +0200

    inject gql to ecma, improve graphql hl

---
+++ .config/nvim/after/queries/ecma/injections.scm
@@ -0,0 +1,6 @@
+; extends
+
+((comment) @_gql_comment
+  (#eq? @_gql_comment "/* GraphQL */")
+  (template_string) @injection.content
+  (#set! injection.language "graphql"))
---
+++ .config/nvim/after/queries/graphql/highlights.scm
@@ -0,0 +1,13 @@
+;; extends
+
+(operation_definition
+  (name) @method)
+
+(argument
+  (name) @variable.builtin)
+
+(comma) @punctuation
+
+(variable_definitions) @variable
+(operation_definition) @variable
+
